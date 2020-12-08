ac594e4106fa736c2dee0dc4f167a4ec287c4274
Chloepottier <chloe.pottier@free.fr>
2020-12-08 12:02:33 +0100

    ajouts de notes

--- curl.php
+++ curl.php
@@ -1,47 +1,45 @@
 <h2>my_bands</h2>
 <?php
-require 'access-token.php';
-$curl = curl_init('https://bandcamp.com/api/account/1/my_bands');
-$headr = array();
+require 'access-token.php'; //access token, refresh_token, client_id et client_secret
+$curl = curl_init('https://bandcamp.com/api/account/1/my_bands');//initise une session cURL
+$headr = array(); //On met les infos à transmettre par le header dans un tableau
 $headr[] = 'Content-length: 0';
-$headr[] = 'Content-type: application/x-www-form-urlencoded';
-$headr[] = 'Authorization: Bearer ' . $accesstoken;
+$headr[] = 'Content-type: application/x-www-form-urlencoded';//format. ne pas mettre json
+$headr[] = 'Authorization: Bearer ' . $accesstoken; // authorization pour la connexion
 $headr[] = 'Accept: application/json';
-$headr[] = 'grant_type: client_credentials';
+$headr[] = 'grant_type: client_credentials'; // important
 $headr[] = 'client_id: ' . $client_id;
 $headr[] = 'client_secret:' . $client_secret;
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headr);
-curl_setopt($curl, CURLOPT_POST, true);
-$data = curl_exec($curl);
-if ($data === false) {
-    var_dump(curl_error($curl . 'ca marche pas'));
-} else {
-    var_dump($data);
-}
-curl_close($curl);
+//ajout d'options cURL
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headr); // tableau $headr est envoyé au header avec la méthode http
+curl_setopt($curl, CURLOPT_POST, true); // cURL post les données du header
+$data = curl_exec($curl);//exécute une session cURl (la requête)
+// if ($data === false) {
+//     var_dump(curl_error($curl . 'ca marche pas'));
+// } else {
+//     var_dump($data);
+ 
+// }
+curl_close($curl); // on ferme la session cURL
 ?>
 <h2>get_merch_details</h2>
 <?php
 // require 'access-token.php';
 $curl2 = 'https://bandcamp.com/api/merchorders/1/get_merch_details';
-$data2 = array("start_time" => "2018-01-01","band_id" => 914283197 );//pour high tone
-
+//tableaux des paramètres de la requête
+$data2 = array("start_time" => "2016-01-01","band_id" => 3390641849 );//pour jarring effects
+//les données doivent être encodé au format json
 $postdata = json_encode($data2);
+//initialisation de curl
 $ch = curl_init($curl2);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POST, 1); // les infos sont transmises en POST
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+//Authorisation par le header directement dans les options de cURL
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $accesstoken,'grant_type: client_credentials','client_id: ' . $client_id,'client_secret:' . $client_secret));
-$result = curl_exec($ch);
-// var_dump($result[]);
-if ($result === false) {
-    var_dump(curl_error($curl2 . 'ca marche pas'));
-} else {
-    var_dump($result);
-}
-curl_close($ch);
+$result = curl_exec($ch);// on execute la session cURL
+curl_close($ch);//on ferme la session cURL
 ?>
-
