b118ff71d4d7557048f11f978fa510e6afe28704
Chloepottier <chloe.pottier@free.fr>
2020-12-29 10:54:27 +0100

    variables à ajouter au woocommerce

--- index.php
+++ index.php
@@ -17,16 +17,25 @@
     $data= json_decode($data);
     echo '<pre>';
     // print_r($data->items[0]->title);//title = totebag 
-    // print_r($data);
+    print_r($data);
     echo '</pre>'; 
     $count_data = count($data->items) -1;//longueur tableau
 
     for ($i = 0; $i <= $count_data; $i++){
-        echo $data->items[$i]->title;
+        $member_band_id = $data->items[$i]->member_band_id ;// a comparer avec band_id pour le nom de l'artiste
+        $title =  $data->items[$i]->title ; // attribut du produit ???
+        $album_title = $data->items[$i]->album_title;
+        $price = $data->items[$i]->price;
+        $image_url = $data->items[$i]->image_url;
+
+        echo '<img src="'.$image_url.'"/>'; //img article
+        echo 'Member id : '. $member_band_id .'<br />' ;// a comparer avec band_id pour le nom de l'artiste
+        echo 'Titre : '. $title .'<br />' ; // attribut du produit ???
+        echo 'Titre de l\'album : '. $album_title .'<br />' ;
+        echo 'Prix : '. $price .' € <br />' ;
+        echo '<img src="'.$image_url.'"/>'; //img article
+        echo '<br />********************<br />';
     }
-   
-
-
     ?>
 
 </body>
