ecc04a3287c6811dff83c7a7e8638f3ae5c9e5df
Chloepottier <chloe.pottier@free.fr>
2020-12-29 18:38:00 +0100

    création des array pour ajouts dans BBD - notations

--- curl.php
+++ curl.php
@@ -1,5 +1,5 @@
 <h2>my_bands</h2>
-<?php
+<?php // récupérer les données
 require 'access-token.php'; //access token, refresh_token, client_id et client_secret
 $curl = curl_init('https://bandcamp.com/api/account/1/my_bands');//initise une session cURL
 $headr = array(); //On met les infos à transmettre par le header dans un tableau
@@ -14,37 +14,5 @@ $headr[] = 'client_secret:' . $client_secret;
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headr); // tableau $headr est envoyé au header avec la méthode http
 curl_setopt($curl, CURLOPT_POST, true); // cURL post les données du header
 $data = curl_exec($curl);//exécute une session cURl (la requête)
-
-
-
 curl_close($curl); // on ferme la session cURL
-// include 'my_bands.php';
-
-
 ?>
-
-
-
-
-
-
-<h2>get_merch_details</h2>
-<?php
-// require 'access-token.php';
-$curl2 = 'https://bandcamp.com/api/merchorders/1/get_merch_details';
-//tableaux des paramètres de la requête
-$data2 = array("start_time" => "2016-01-01","band_id" => 3390641849 );//pour jarring effects
-//les données doivent être encodé au format json
-$postdata = json_encode($data2);
-//initialisation de curl
-$ch = curl_init($curl2);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_POST, 1); // les infos sont transmises en POST
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-//Authorisation par le header directement dans les options de cURL
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $accesstoken,'grant_type: client_credentials','client_id: ' . $client_id,'client_secret:' . $client_secret));
-$result = curl_exec($ch);// on execute la session cURL
-curl_close($ch);//on ferme la session cURL
--- index.php
+++ index.php
@@ -20,12 +20,16 @@
     echo '</pre>'; 
     $count_data = count($data->items) -1;//longueur tableau
     $arrayProducts = [];
+    $post_id = 4800;
+
     for ($i = 0; $i <= $count_data; $i++){
         $member_band_id = $data->items[$i]->member_band_id ;// a comparer avec band_id pour le nom de l'artiste
         $title =  $data->items[$i]->title ; // attribut du produit ???
         $album_title = $data->items[$i]->album_title;
         $price = $data->items[$i]->price;
         $image_url = $data->items[$i]->image_url;
+        $post_id++;
+
         echo '<img src="'.$image_url.'"/><br />'; //img article
         // echo $arrayBands[0][0];
         $count_bands = count($arrayBands) -1;
@@ -34,23 +38,55 @@
                 $band_name = $arrayBands[$i2][2];
                 echo '<h1>Artiste :'. $band_name .'</h1>';
             } 
+            
         }
         if ( $member_band_id == 3390641849){
             $band_name = 'Jarring Effects label';
             echo '<h1>Artiste : '.$band_name.'</h1>';
         }
         echo 'Member id : '. $member_band_id .'<br />' ;// a comparer avec band_id pour le nom de l'artiste
-        //echo 'Titre : '. $title .'<br />' ; // attribut du produit ???
+        echo 'Titre : '. $title .'<br />' ; // attribut du produit ???
         echo 'Titre de l\'album : '. $album_title .'<br />' ;
         echo 'Prix : '. $price .' € <br />' ;
         echo '<img src="'.$image_url.'"/>'; //img article
         echo '<br />********************<br />';
-        $arrayProducts[] = [$member_band_id, $band_name,$album_title,$price,$image_url ];
+        if( !isset($album_title)){  //post_type = 'product'
+            $arrayProducts[] = [$post_id,$member_band_id, $band_name,$album_title, $title, 'ajout du produit']; // tableau des produits qui n'existe qu'en 1 seul format
+        } else{
+            $arrayFormatSupp[] = [$post_id,$band_name, $album_title, $title, 'autre formats']; // tableau des produits qui existe en plusieurs formats
+        }
+        // $arrayProducts[] = [$post_id,$member_band_id, $band_name,$album_title,$price, $title];  //post_type = 'product'
+
+        $post_parent = $post_id;
+        $post_id++;
+        $arrayAttachment[] = [$post_id,$image_url,$post_parent]; // post-type = 'attachment' -> les url des images bandcamp. si ca marche pas dans média, remettre attachment dans le tableau pr
+        // $posts[] = [$post_id,$member_band_id,$album_title]; //champs à insérer dans la table wp_posts
+        // $post_meta_details[] = ['post-id' => $post_id , 'meta' => [$band_name,$price,$image_url,$title]]; //champs à insérer dans la table wp_post_meta
+
+
+        // revoir postmeta à cause de $arrayFormatSupp (rajouté à la fin , comporte tous ce qui existe en plusieurs fois)
+        $post_meta[] = [
+            [$post_id,$band_name],
+            [$post_id,$price], //meta_key = _price
+            [$post_id,$title]
+        ];
     }
-    echo '<h2>tableau pour BDD</h2>';
+    echo '<h2>Array Products : post-type "product"</h2>';
     echo '<pre>';
     print_r($arrayProducts);
-    echo '</pre>';         ?>
+    echo '</pre>'; 
+    echo '<h2>Array Attachment : post-type "attachment", lié à un post parent(product)</h2>';
+    echo '<pre>';
+    print_r($arrayAttachment);
+    echo '</pre>';    
+    
+    echo '<h2>Array autre formats (ajout manuel)</h2>';
+    echo '<pre>';
+    print_r($arrayFormatSupp );
+    echo '</pre>';
+    // include 'insertion-BDD.php';
+  
+    ?>
 </body>
 
 </html>
